a44c59fa53474a513a5f80d377138b18f3e9cfa8
helifreak <heli.freak.95@gmail.com>
2017-03-07 10:07:39 +0100

    greeny is too literal

--- modules.py
+++ modules.py
@@ -3,6 +3,6 @@ import sopel.module
 @sopel.module.commands('modules')
 def modules(bot, trigger):
     if trigger.group(3) == 'help':
-        bot.say('admin and adminchannel are built in, all others have !module help available')
+        bot.say('find, admin and adminchannel are built in, reload, and op are admin only, all others have \'!<module> help\' available')
     else:
         bot.say('Modules loaded: ' + ', '.join(bot.config.core.enable))
--- title.py
+++ title.py
@@ -58,7 +58,7 @@ def title(bot, trigger):
         bot.db.set_nick_value(trigger.nick, 'title', 'off')
         bot.say('Titles disabled', trigger.nick)
     elif trigger.group(3) == 'help':
-        bot.say('Sends link titles via privmsg/notice (!title on/off/privmsg/notice/status')
+        bot.say('Sends link titles via privmsg/notice (!title on/off/privmsg/notice/status)')
 
 @sopel.module.rule('(?u).*(https?://\S+).*')
 def title_auto(bot, trigger):
