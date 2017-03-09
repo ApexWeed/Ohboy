6834b46e61a4a314198004a0fcfe77b71e1f2494
helifreak <heli.freak.95@gmail.com>
2017-03-09 10:56:11 +0100

    Sped up help text output

--- help.py
+++ help.py
@@ -4,13 +4,14 @@ import collections
 helptext = collections.namedtuple('HelpText', 'perms command line')
 
 def setup(bot):
-    #if not bot.memory.contains('help'):
-    bot.memory['help'] = sopel.tools.SopelMemory()
-    bot.memory['help']['test'] = sopel.tools.SopelMemory()
-    bot.memory['help']['test']['short'] = 'Help text for test module'
-    bot.memory['help']['test']['long'] = {helptext('all', '!test', 'Does nothing'), helptext('owner', '!wow', 'Wows'), helptext('user', '!indeed', 'Successful test minion')}
-    bot.memory['help']['nice'] = sopel.tools.SopelMemory()
-    bot.memory['help']['nice']['short'] = 'Help text for nice module'
+    if not bot.memory.contains('help'):
+        bot.memory['help'] = sopel.tools.SopelMemory()
+
+def say_direct(bot, target, message):
+    args = ('PRIVMSG', target)
+    bot.write(args, message[:400])
+    if len(message) > 400:
+        say_direct(bot, target, message)
 
 @sopel.module.commands('help')
 def help(bot, trigger):
@@ -20,13 +21,13 @@ def help(bot, trigger):
     module = trigger.group(3)
     if not module or module == 'all':
         for key, value in bot.memory['help'].items():
-            bot.say('%s: %s' % (key, value['short']))
+            say_direct(bot, trigger.nick, '%s: %s' % (key, value['short']))
     elif module in bot.memory['help']:
         if 'long' in bot.memory['help'][module]:
             count = 0
             for cmd in bot.memory['help'][module]['long']:
                 if cmd.perms == 'all' or (cmd.perms == 'admin' and trigger.admin) or (cmd.perms == 'owner' and trigger.owner) or (cmd.perms == 'user' and not trigger.admin):
-                    bot.say('%s: %s' % (cmd.command, cmd.line))
+                    say_direct(bot, trigger.nick, '%s: %s' % (cmd.command, cmd.line))
                     count = count + 1
             if count == 0:
                 bot.say('No extended help available for %s' % module)
