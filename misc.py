e6d32e51c09d76ad2b6afd9545900abbdecd5f82
helifreak <heli.freak.95@gmail.com>
2017-04-12 13:51:36 +0200

    Add jiggle

--- misc.py
+++ misc.py
@@ -3,3 +3,11 @@ import sopel.modules
 @sopel.module.rule(r'ping.*')
 def pong(bot, trigger):
     bot.say('pong')
+
+@sopel.module.rule(r'.*boob.*')
+def boobs(bot, trigger):
+    bot.action('jiggles')
+
+@sopel.module.rule(r'^\.bang')
+def bang(bot, trigger):
+    bot.say('{} is a bad guy'.format(trigger.nick))
